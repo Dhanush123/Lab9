333bc04a4eea93fac38ec4fe061e1791ca39f83c
tejashah88 <tejashah88@gmail.com>
2015-10-20 22:24:16 -0700

    Added NSA Encoder
    added NSA Encoder for 9.2

---
+++ nsaEncoder0.cpp
@@ -0,0 +1,24 @@
+// Lab 9
+// Programmer: Dhanush Patel
+// Editor(s) used: Eclipse
+// Compiler(s) used: Eclipse
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "Programmer: Dhanush Patel" << endl;
+	cout << "Description: This program encodes a string based off of what the user inputs." << endl;
+    cout << endl;
+	
+	string input;
+	
+	cout << "Enter a line of text: ";
+	getline(cin, input);
+	
+	for (int i = 0; i < input.length(); i++)
+		input[i]++;
+	
+	cout << input << endl;
+}
