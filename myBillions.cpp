89759a479b4d4b24b0087d4ade19349814a1569c
Dhanush123 <dhanush.patel@ymail.com>
2015-11-16 00:40:58 -0800

    bug fix
    int overflow fix

--- myBillions.cpp
+++ myBillions.cpp
@@ -11,45 +11,45 @@ int main() {
 	cout << "Programmer: Dhanush Patel" << endl;
 	cout << "Description: This program calculates when you can be a billionaire." << endl;
     cout << endl;
-	
-	const int thousandaireCentsAmount = 1000 * 100;
-	const int millionaireCentsAmount  = 1000000 * 100;
-	const int billionaireCentsAmount  = 1000000000 * 100;
-	
+
+	const int thousandaireAmount = 1000;
+	const int millionaireAmount  = 1000000;
+	const int billionaireAmount  = 1000000000;
+
 	bool hasThousandaireCalcs = false;
 	bool hasMillionaireCalcs = false;
 	bool hasBillionaireCalcs = false;
-	
+
 	int weeksToThousandaire;
 	int weeksToMillionaire;
 	int weeksToBillionaire;
-	
-	int amount = 0;
-	int initial = 5;
-	int currentWeekNumber = 1;
-	
+
+	double amount = 0;
+	double initial = 0.05;
+	int currentWeekNumber = 0;
+
 	while (true) {
-		if (!hasThousandaireCalcs && amount >= thousandaireCentsAmount) {
+		if (!hasThousandaireCalcs && amount >= thousandaireAmount) {
 			hasThousandaireCalcs = true;
 			weeksToThousandaire = currentWeekNumber;
 		}
-		
-		if (!hasMillionaireCalcs && amount >= millionaireCentsAmount) {
+
+		if (!hasMillionaireCalcs && amount >= millionaireAmount) {
 			hasMillionaireCalcs = true;
 			weeksToMillionaire = currentWeekNumber;
 		}
-		
-		if (!hasBillionaireCalcs && amount >= billionaireCentsAmount) {
+
+		if (!hasBillionaireCalcs && amount >= billionaireAmount) {
 			hasBillionaireCalcs = true;
 			weeksToBillionaire = currentWeekNumber;
 			break;
 		}
-		
+
 		amount += initial;
 		initial *= 2;
 		currentWeekNumber++;
 	}
-	
+
 	cout << "In " << weeksToThousandaire << " weeks, I will be a thousandaire!" << endl;
 	cout << "In " << weeksToMillionaire << " weeks, I will be a millionaire!" << endl;
 	cout << "In " << weeksToBillionaire << " weeks, I will be a billionaire!" << endl;
